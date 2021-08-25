a8534f534f61adaad4f193d7a38606e4b50a475b
xiaoyaosheng <zhengxiaoch@163.com>
2021-08-25 21:45:01 +0800

    修改配置

--- docs/zh/DevAdvance/sidebar.js
+++ docs/zh/DevAdvance/sidebar.js
@@ -3,10 +3,10 @@ module.exports = [
   {
     title: 'SpringBoot基础',
     children: [
-        '/zh/DevAdvance/SpringBoot/basic/01-第一个 Spring Boot 应用程序',
-        '/zh/DevAdvance/SpringBoot/basic/02-Spring Boot 单元测试',
+      '/zh/DevAdvance/SpringBoot/basic/01-第一个 Spring Boot 应用程序',
+      '/zh/DevAdvance/SpringBoot/basic/02-Spring Boot 单元测试',
       '/zh/DevAdvance/SpringBoot/basic/03-Spring Boot 常用配置',
-      ]
+    ]
   },
   {
     title: 'SpringBoot整合',
@@ -14,15 +14,15 @@ module.exports = [
     children: [
       '/zh/DevAdvance/SpringBoot/Integration/01-集成Mybatis通用mapper和pagehelper',
       '/zh/DevAdvance/SpringBoot/Integration/02-全局异常处理',
-    '/zh/DevAdvance/SpringBoot/Integration/03-JSR-303实现请求参数校验',
-    '/zh/DevAdvance/SpringBoot/Integration/04-整合Swagger2生成API文档',
-    '/zh/DevAdvance/SpringBoot/Integration/05-集成JWT实现token验证',
-    '/zh/DevAdvance/SpringBoot/Integration/06-集成定时任务',
-    '/zh/DevAdvance/SpringBoot/Integration/07-集成邮件服务',
-    '/zh/DevAdvance/SpringBoot/Integration/08-整合Redis',
-    '/zh/DevAdvance/SpringBoot/Integration/09-整合阿里云短信',
-    '/zh/DevAdvance/SpringBoot/Integration/10-整合SpringSecurity实现权限注解+JWT登录认证',
-    '/zh/DevAdvance/SpringBoot/Integration/11-整合SpringRetry',
+      '/zh/DevAdvance/SpringBoot/Integration/03-JSR-303实现请求参数校验',
+      '/zh/DevAdvance/SpringBoot/Integration/04-整合Swagger2生成API文档',
+      '/zh/DevAdvance/SpringBoot/Integration/05-集成JWT实现token验证',
+      '/zh/DevAdvance/SpringBoot/Integration/06-集成定时任务',
+      '/zh/DevAdvance/SpringBoot/Integration/07-集成邮件服务',
+      '/zh/DevAdvance/SpringBoot/Integration/08-整合Redis',
+      '/zh/DevAdvance/SpringBoot/Integration/09-整合阿里云短信',
+      '/zh/DevAdvance/SpringBoot/Integration/10-整合SpringSecurity实现权限注解+JWT登录认证',
+      '/zh/DevAdvance/SpringBoot/Integration/11-整合SpringRetry',
     ]
   },
   {
@@ -31,30 +31,30 @@ module.exports = [
     children: [
       '/zh/DevAdvance/SpringBoot/SpringSecurity/01-SpringSecurity自定义基本使用及个性化登录配置',
       '/zh/DevAdvance/SpringBoot/SpringSecurity/02-SpringSecurity安全认证流程源码详解',
-    '/zh/DevAdvance/SpringBoot/SpringSecurity/03-SpringSecurity实现图片验证码认证',
-    '/zh/DevAdvance/SpringBoot/SpringSecurity/04-SpringSecurity记住我功能实现',
-    '/zh/DevAdvance/SpringBoot/SpringSecurity/05-实现短信验证码+登录功能',
-    '/zh/DevAdvance/SpringBoot/SpringSecurity/SpringSecurity OAuth2.0认证授权',
+      '/zh/DevAdvance/SpringBoot/SpringSecurity/03-SpringSecurity实现图片验证码认证',
+      '/zh/DevAdvance/SpringBoot/SpringSecurity/04-SpringSecurity记住我功能实现',
+      '/zh/DevAdvance/SpringBoot/SpringSecurity/05-实现短信验证码+登录功能',
+      '/zh/DevAdvance/SpringBoot/SpringSecurity/SpringSecurity OAuth2.0认证授权',
     ]
   },
   {
     title: '编程方法论',
     children: [
-        '/zh/DevAdvance/ProgramMethodology/01-敏捷宣言',
-        '/zh/DevAdvance/ProgramMethodology/02-敏捷开发',
+      '/zh/DevAdvance/ProgramMethodology/01-敏捷宣言',
+      '/zh/DevAdvance/ProgramMethodology/02-敏捷开发',
       '/zh/DevAdvance/ProgramMethodology/03-极限编程',
       '/zh/DevAdvance/ProgramMethodology/04-微服务十二要素宣言',
       '/zh/DevAdvance/ProgramMethodology/05-无状态应用',
       '/zh/DevAdvance/ProgramMethodology/06-轻应用',
       '/zh/DevAdvance/ProgramMethodology/07-禅道简介',
       '/zh/DevAdvance/ProgramMethodology/08-安装禅道',
-      ]
+    ]
   },
   {
     title: '微服务',
     children: [
-        '/zh/DevAdvance/MicroService/01-构建单体应用模型',
-        '/zh/DevAdvance/MicroService/02-走向单体地狱',
+      '/zh/DevAdvance/MicroService/01-构建单体应用模型',
+      '/zh/DevAdvance/MicroService/02-走向单体地狱',
       '/zh/DevAdvance/MicroService/03-微服务解决复杂问题',
       '/zh/DevAdvance/MicroService/04-微服务的优点',
       '/zh/DevAdvance/MicroService/05-微服务的缺点',
@@ -63,13 +63,25 @@ module.exports = [
       '/zh/DevAdvance/MicroService/08-微服务的概念',
       '/zh/DevAdvance/MicroService/09-微服务的实践',
       '/zh/DevAdvance/MicroService/10-微服务设计模式',
-      ]
+    ]
+  },
+  {
+    title: '微服务技术运用',
+    children: [
+      '/zh/DevAdvance/MicroServiceSkill/SpringCloud01/SpringCloud01',
+      '/zh/DevAdvance/MicroServiceSkill/SpringCloud02/SpringCloud实用篇02',
+      '/zh/DevAdvance/MicroServiceSkill/Docker/Docker实用篇',
+      '/zh/DevAdvance/MicroServiceSkill/MQ/RabbitMQ',
+      '/zh/DevAdvance/MicroServiceSkill/SpringCloud01/分布式搜索引擎01',
+      '/zh/DevAdvance/MicroServiceSkill/SpringCloud02/分布式搜索引擎02',
+      '/zh/DevAdvance/MicroServiceSkill/SpringCloud03/分布式搜索引擎03',
+    ]
   },
   {
     title: 'Docker容器',
     children: [
-        '/zh/DevAdvance/Docker/Docker/Docker简介',
-        '/zh/DevAdvance/Docker/Docker/安装Docker',
+      '/zh/DevAdvance/Docker/Docker/Docker简介',
+      '/zh/DevAdvance/Docker/Docker/安装Docker',
       '/zh/DevAdvance/Docker/Docker/Docker 概述',
       '/zh/DevAdvance/Docker/Docker/Docker 操作镜像',
       '/zh/DevAdvance/Docker/Docker/Docker 操作容器',
@@ -85,13 +97,13 @@ module.exports = [
       '/zh/DevAdvance/Docker/Docker Compose/Docker Compose 部署 Nginx',
       '/zh/DevAdvance/Docker/Docker Compose/Docker Compose 部署 Portainer',
       '/zh/DevAdvance/Docker/Docker Compose/Docker Compose 网络设置',
-      ]
+    ]
   },
   {
     title: 'Kubernetes',
     children: [
-        '/zh/DevAdvance/Kubernetes/01-Kubernetes 简介',
-        '/zh/DevAdvance/Kubernetes/02-Kubernetes 安装前的准备',
+      '/zh/DevAdvance/Kubernetes/01-Kubernetes 简介',
+      '/zh/DevAdvance/Kubernetes/02-Kubernetes 安装前的准备',
       '/zh/DevAdvance/Kubernetes/03-Kubernetes 安装集群',
       '/zh/DevAdvance/Kubernetes/04-Kubernetes 配置网络',
       '/zh/DevAdvance/Kubernetes/05-Kubernetes 第一个容器',
@@ -106,15 +118,15 @@ module.exports = [
       '/zh/DevAdvance/Kubernetes/14-Kubernetes 使用数据卷',
       '/zh/DevAdvance/Kubernetes/15-Kubernetes ConfigMap',
       '/zh/DevAdvance/Kubernetes/16-Kubernetes Dashboard',
-      ]
+    ]
   },
   {
     title: '解决方案',
     children: [
-        '/zh/DevAdvance/Solution/login/01-从零开始设计一个登录系统',
-        '/zh/DevAdvance/Solution/login/02-关于用户系统中第三方登录的设计',
+      '/zh/DevAdvance/Solution/login/01-从零开始设计一个登录系统',
+      '/zh/DevAdvance/Solution/login/02-关于用户系统中第三方登录的设计',
       '/zh/DevAdvance/Solution/login/03-第三方账号登录',
       '/zh/DevAdvance/Solution/login/04-Spring Boot 快速集成第三方登录功能',
-      ]
+    ]
   }
 ];
